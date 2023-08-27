686d2ec2885de486b111bfefd8157229b8781168
Alvaro Jofre <a.jofrema@gmail.com>
2023-08-27 05:01:05 -0400

    comet

--- skeletonApp/src/app/access/access.page.ts
+++ skeletonApp/src/app/access/access.page.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-access',
   templateUrl: 'access.page.html',
   styleUrls: ['access.page.scss']
 })
-
 export class AccessPage {
-    // Código para tu página de acceso, si necesitas algo específico.
+  
+  constructor(private actionSheetController: ActionSheetController) {}
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Comandos',
+      buttons: [{
+        text: 'Opción 1',
+        handler: () => {
+          console.log('Opción 1 clicked');
+        }
+      }, {
+        text: 'Opción 2',
+        handler: () => {
+          console.log('Opción 2 clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  // Función para manejar el evento click del botón "templates"
+  handleTemplates() {
+    console.log('Botón templates presionado');
+  }
+
+  // Función para manejar el evento click del botón "agendamiento"
+  handleAgendamiento() {
+    console.log('Botón agendamiento presionado');
+  }
+
 }
--- skeletonApp/src/app/home/home.page.ts
+++ skeletonApp/src/app/home/home.page.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-
 export class HomePage {
-    // Código para tu página de inicio, si necesitas algo específico.
+
+  constructor(private actionSheetController: ActionSheetController) {}
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Comandos',
+      buttons: [{
+        text: 'GV55',
+        handler: () => {
+          console.log('Opción 1 clicked');
+        }
+      }, {
+        text: 'GV75',
+        handler: () => {
+          console.log('Opción 2 clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+  
+  // Otros métodos y lógica pueden seguir aquí...
 }

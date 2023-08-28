1c52bb08e3e35e40355a82e1c8138faa35b30c13
Alvaro Jofre <a.jofrema@gmail.com>
2023-08-28 01:29:21 -0400

    fff

--- skeletonApp/src/app/home/home.page.ts
+++ skeletonApp/src/app/home/home.page.ts
@@ -3,8 +3,8 @@ import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
 })
 export class HomePage {
   
